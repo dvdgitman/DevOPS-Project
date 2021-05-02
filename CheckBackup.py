969a0332b6879cd6ba390e29e25574f066624315
David Gitman <wereking@gmail.com>
2021-05-02 19:11:14 +0300

    changed some html code

--- CheckBackup.py
+++ CheckBackup.py
@@ -12,10 +12,17 @@ def homepage():
 
 
 @app.route('/data')
-def data():
+def data(urls):
     if request.method == 'GET':
+        try:
+            r = requests.get(urls)
+            print(urls + "\tStatus: " + str(r.status_code))  # Prints the url and status of the site(Up/Down)
+        except Exception as e:
+            print(urls + " - The url is probably down or isn't written correctly.Make sure to add http or https. ")
         return render_template('data.html')
+    # if request.method != 'GET':
+    #     return render_template('url.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
