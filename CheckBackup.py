cda0004532e19cd702e9770a25471480eb21966a
David Gitman <wereking@gmail.com>
2021-05-02 22:36:19 +0300

    dont even ask

--- CheckBackup.py
+++ CheckBackup.py
@@ -1,30 +1,27 @@
 # importing Flask and other modules
-from flask import Flask, request, render_template
-import cgi
-import requests
+from flask import Flask, request, render_template, redirect, url_for
 import webbrowser
 
-form = cgi.FieldStorage()
 # Flask constructor
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
-    """Run a web UI."""
     return render_template("index.html")
 
 
-@app.route('/data', methods=['POST', 'GET'])
+@app.route('/data/', methods=['POST', 'GET'])
 def data():
     if request.method == "GET":
-        return ()
-        site = form.getvalue('name')
-        #url = requests.get(site)
-        return render_template('data.html')
-        #print(site.status_code)
+        url = request.form["url"]
+        return redirect(url_for("data"))
+    else:
+        return render_template('url.html')
     # if request.get != 'url':
     #     return render_template('url.html')
+
+
 
 
 if __name__ == '__main__':
