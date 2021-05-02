7fed54595451b4ee45c019dda0df8719d9c53d94
David Gitman <wereking@gmail.com>
2021-05-02 19:00:04 +0300

    changed backup a little bit

--- CheckBackup.py
+++ CheckBackup.py
@@ -1,5 +1,5 @@
 # importing Flask and other modules
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, make_response
 import requests
 
 # Flask constructor
@@ -10,9 +10,11 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
+
 @app.route('/data')
 def data():
-    return render_template('test.html')
+    if request.method == 'GET':
+        return render_template('data.html')
 
 
 if __name__ == '__main__':
